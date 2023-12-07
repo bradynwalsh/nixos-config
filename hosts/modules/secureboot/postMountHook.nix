aeb12c582f194322530b512a4470536e1b830537
Bradyn Walsh <bradyn.walsh@gmail.com>
2023-12-07 16:21:43 +0000

    Use yolo enroll-keys mode for secureboot

--- hosts/modules/secureboot/postMountHook.nix
+++ hosts/modules/secureboot/postMountHook.nix
@@ -3,7 +3,7 @@
 # Create and Enroll SecureBoot Keys on partition creation
 ''
 ${pkgs.sbctl}/bin/sbctl create-keys
-${pkgs.sbctl}/bin/sbctl enroll-keys -t
+${pkgs.sbctl}/bin/sbctl enroll-keys --yes-this-might-brick-my-machine
 
 mkdir /mnt/persist/secureboot
 cp /etc/secureboot /mnt/persist/secureboot
