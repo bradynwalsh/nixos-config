d12845cf5a87fe078e09877a5e69f441030fcdcc
Bradyn Walsh <bradyn.walsh@gmail.com>
2023-12-07 16:06:30 +0000

    Fix sbctl binary path

--- hosts/modules/secureboot/postMountHook.nix
+++ hosts/modules/secureboot/postMountHook.nix
@@ -2,8 +2,8 @@
 
 # Create and Enroll SecureBoot Keys on partition creation
 ''
-${pkgs.sbctl} create-keys
-${pkgs.sbctl} enroll-keys -t
+${pkgs.sbctl}/bin/sbctl create-keys
+${pkgs.sbctl}/bin/sbctl enroll-keys -t
 
 mkdir /mnt/persist/secureboot
 cp /etc/secureboot /mnt/persist/secureboot
