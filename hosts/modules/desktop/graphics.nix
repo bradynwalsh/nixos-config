6f5cc7e31399274cb18a42655672d6a87d4d5ad1
Bradyn Walsh <bradyn.walsh@gmail.com>
2023-12-11 08:24:00 +0000

    Graphical boot

--- hosts/modules/desktop/graphics.nix
+++ hosts/modules/desktop/graphics.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
+  # Graphical boot process
+  boot.plymouth.enable = true;
+  boot.initrd.verbose = false;
+  boot.consoleLogLevel = 0;
+  boot.kernelParams = [ "quiet" "udev.log_level=3" ];
+
   # Use Hyprland package from flake
   programs.hyprland = {
     enable = true;
