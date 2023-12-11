773fb9326310adbc848ec44384f7371128bb5755
Bradyn Walsh <bradyn.walsh@gmail.com>
2023-12-11 04:44:17 -0700

    auto-sync timezone

--- hosts/modules/common/time.nix
+++ hosts/modules/common/time.nix
@@ -4,4 +4,7 @@
   # Use chronyd instead of systemd-timesyncd
   services.chrony.enable = true;
   services.timesyncd.enable = false;
+
+  # Automatic timezone sync
+  services.automatic-timezoned.enable = true;
 }
