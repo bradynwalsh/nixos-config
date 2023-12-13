352be4fd4c4f6538957978248871919f7ac6515a
Bradyn Walsh <bradyn.walsh@gmail.com>
2023-12-12 20:03:17 -0700

    Only use NetworkManager

--- hosts/modules/desktop/networking.nix
+++ hosts/modules/desktop/networking.nix
@@ -1,11 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Enable Wi-Fi
-  networking.wireless.enable = true;
-  networking.wireless.dbusControlled = true;
-  networking.wireless.userControlled.enable = true;
-
   # Use NetworkManager
   networking.networkmanager.enable = true;
 }
