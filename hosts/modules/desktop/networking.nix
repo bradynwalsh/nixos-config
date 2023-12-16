e075f9fab41c3e3a2f3278ea80c0b8c465bfb91b
Bradyn Walsh <bradyn.walsh@gmail.com>
2023-12-16 04:17:31 -0700

    Persist network connections

--- hosts/modules/desktop/networking.nix
+++ hosts/modules/desktop/networking.nix
@@ -3,4 +3,12 @@
 {
   # Use NetworkManager
   networking.networkmanager.enable = true;
+
+  environment.persistence."/persist/networking" = {
+    hideMounts = true;
+
+    directories = [
+      "/etc/NetworkManager/system-connections"
+    ];
+  };
 }
