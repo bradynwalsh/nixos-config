1bfbe39bd90aeffddaedfbcd5f45e02e75375e1c
Bradyn Walsh <bradyn.walsh@gmail.com>
2023-12-22 22:01:58 +1030

    Set default shell to fish

--- hosts/modules/common/corePackages.nix
+++ hosts/modules/common/corePackages.nix
@@ -3,8 +3,11 @@
 {
   programs.git.enable = true;
   programs.ssh.startAgent = true;
+  programs.fish.enable = true;
 
   environment.systemPackages = [
     pkgs.ripgrep
   ];
+
+  users.defaultUserShell = pkgs.fish;
 }
