8d77c29dc530b95073841c1073985e33c051047e
Bradyn Walsh <bradyn.walsh@gmail.com>
2024-02-07 11:34:23 +1030

    VSCode Configuration

--- users/applications/vscode.nix
+++ users/applications/vscode.nix
@@ -3,5 +3,14 @@
 {
   programs.vscode = {
     enable = true;
+
+    userSettings = {
+      "window.menuBarVisibility" = "toggle";
+    };
+
+    extensions = [
+      pkgs.vscode-extensions.bbenoist.nix
+      pkgs.vscode-extensions.tomoki1207.pdf
+    ];
   };
 }
