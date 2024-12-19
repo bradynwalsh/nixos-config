a05c4036cf837c94bb0590d62b1e84e1036eb59a
Bradyn Walsh <bradyn.walsh@gmail.com>
2024-12-19 12:54:04 +1030

    Actually fix fonts

--- users/modules/desktop/fonts.nix
+++ users/modules/desktop/fonts.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs ; [
     jetbrains-mono
-    nerdfonts.jetbrains-mono
+    nerd-fonts.jetbrains-mono
     nerd-fonts.iosevka
     nerd-fonts.iosevka-term
     nerd-fonts.iosevka-term-slab
