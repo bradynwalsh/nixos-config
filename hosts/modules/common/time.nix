db2eeff3957f5f6d0ab76ddf97dc6e10d08bb2e1
Bradyn Walsh <bradyn.walsh@gmail.com>
2024-12-21 18:41:27 +1030

    Fixed geolocation
    Changed from MLS to BeaconDB to fix geolocation/timezone detection

--- hosts/modules/common/time.nix
+++ hosts/modules/common/time.nix
@@ -8,6 +8,9 @@
   # Automatic timezone sync
   services.automatic-timezoned.enable = true;
 
+  # Use BeaconDB for geolocation (MLS default has shut down)
+  services.geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+
   # Give Geoclue access to NetworkManager (if installed) for more accurate geo-location
   users.users.geoclue.extraGroups = [ "networkmanager" ];
 }
