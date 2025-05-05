89201d52356dfe0baaf13a6a17e9c1e3410dd87d
Bradyn Walsh <bradyn.walsh@gmail.com>
2025-05-05 22:53:57 +1000

    Change to refactored mako HM config

--- users/modules/desktop/mako.nix
+++ users/modules/desktop/mako.nix
@@ -4,32 +4,38 @@
   services.mako = {
     enable = true;
 
-    font = "JetBrains Mono 12";
-    backgroundColor = "#2B3339CC";
+    settings = {
+      font = "JetBrains Mono 12";
+      backgroundColor = "#2B3339CC";
 
-    width = 300;
-    height = 110;
+      width = "300";
+      height = "110";
 
-    borderSize = 2;
-    borderColor = "#4C566A";
-    borderRadius = 18;
+      borderSize = "2";
+      borderColor = "#4C566A";
+      borderRadius = "18";
 
-    defaultTimeout = 60000;
+      defaultTimeout = "60000";
+    };
 
-    extraConfig = ''
-      [urgency=low]
-      border-color=#cccccc
+    criteria = {
+      "urgency=low" = {
+        border-color = "#cccccc";
+      };
 
-      [urgency=normal]
-      border-color=#d08770
+      "urgency=normal" = {
+        border-color = "#d08770";
+      };
 
-      [urgency=high]
-      border-color=#bf616a
-      default-timeout=0
+      "urgency=high" = {
+        border-color = "#bf616a";
+        default-timeout = "0";
+      };
 
-      [category=mpd]
-      default-timeout=20000
-      group-by=category
-    '';
+      "category=mpd" = {
+        default-timeout = "20000";
+        group-by = "category";
+      };
+    };
   };
 }
