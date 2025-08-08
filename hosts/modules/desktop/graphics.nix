33d1e2f8fd249de2f90fff8c35ae052546ded8ac
Bradyn Walsh <bradyn.walsh@gmail.com>
2025-08-08 21:39:59 +0930

    Use pkgs.tuigreet instead of pkgs.greetd.tuigreet
    Refactor to reflect upstream nixpkgs change

--- hosts/modules/desktop/graphics.nix
+++ hosts/modules/desktop/graphics.nix
@@ -30,7 +30,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time -r --cmd Hyprland --user-menu --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time -r --cmd Hyprland --user-menu --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red";
         user = "greeter";
       };
     };
