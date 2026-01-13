855194bae9e5089cba8bc0d388f1c9361c06f695
Bradyn Walsh <bradyn.walsh@gmail.com>
2026-01-14 00:31:21 +1030

    Fix hyprland option changes

--- users/modules/desktop/hyprland.nix
+++ users/modules/desktop/hyprland.nix
@@ -142,10 +142,7 @@
       };
 
       input-field = {
-        size = {
-          width = 250;
-          height = 60;
-        };
+        size = "250, 60";
         outline_thickness = 2;
         dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
         dots_spacing = 0.2 ;# Scale of dots' absolute size, 0.0 - 1.0
@@ -194,8 +191,11 @@
   ];
 
   config.xdg.configFile."hypr/hyprpaper.conf".text = ''
-    preload = ${./wallpaper.png}
-    wallpaper = ,${./wallpaper.png}
+    wallpaper {
+      monitor =
+      path = ${./wallpaper.png}
+      fit_mode = fill
+    }
   '';
 
   config.gtk.iconTheme = {
